28813a4dbd520ec6f4a6462d7a9602489c7ea8c4
Mike <nhj25252@gmail.com>
2023-01-25 19:04:21 +0900

    Create 16 by 16 grid

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-for (let i = 0; i <= 16; i++) {
-  const container = document.querySelector(".container");
-  const divRow = document.createElement("div");
-  container.appendChild(divRow);
-  for (let k = 0; k <= 16; k++) {
-    const divColumn = document.createElement("div");
-    container.appendChild(divColumn);
-    divColumn.setAttribute(
-      "style",
-      "height: 50px; width: 50px; border: thick solid black;"
-    );
-  }
-  divRow.setAttribute(
-    "style",
-    "height: 50px; width: 50px; border: thick solid black;"
-  );
+const container = document.querySelector(".container");
+container.setAttribute(
+  "style",
+  "display: flex; max-width:20em; max-height: 100em; flex-wrap: wrap;"
+);
+
+for (let i = 0; i <= 16 * 16 - 1; i++) {
+  const div = document.createElement("div"); //create a div in memory
+  container.appendChild(div); //append div to the front end
+  div.classList.add("square");
+
+  div.setAttribute("style", "width:1em; height:1em"); //why is this working?
 }
+const allDivs = document.querySelectorAll(".square");
+allDivs.setAttribute("style", "flex-shrink:1;");
