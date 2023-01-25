670343be8009093ad9b3b6f59be37a4ddf1cdebc
Mike <nhj25252@gmail.com>
2023-01-26 00:52:39 +0900

    Add a hovering effect

--- index.js
+++ index.js
@@ -8,8 +8,24 @@ for (let i = 0; i <= 16 * 16 - 1; i++) {
   const div = document.createElement("div"); //create a div in memory
   container.appendChild(div); //append div to the front end
   div.classList.add("square");
-
   div.setAttribute("style", "width:1em; height:1em"); //why is this working?
 }
-//const allDivs = document.querySelectorAll(".square");
-//allDivs.setAttribute("style", "flex-shrink:1;");
+
+const divs = document.querySelectorAll(".square"); //divs is a Nodelist! not an array!
+const divsArr = Array.from(divs);
+
+for (i = 0; i < divsArr.length; i++) {
+  divsArr[i].addEventListener("mouseover", changeBackground);
+}
+
+function changeBackground(event) {
+  event.target.style.backgroundColor = "blue";
+}
+
+/*addEventListener("mouseover", (event) => {
+  divsArr.target.style.background = "blue";
+});*/
+/*function hover() {
+  divsArr.addEventListener("mouseover", )
+  divsArr.setAttribute("style", "background:blue;");
+}*/
