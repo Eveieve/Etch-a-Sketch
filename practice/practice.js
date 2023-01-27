69e288d1ae651ab78798d73269f14a81537e0f80
Mike <nhj25252@gmail.com>
2023-01-27 21:21:15 +0900

    Remove unwanted files

--- practice/practice.js
+++
@@ -1,8 +0,0 @@
-// To-do list ul element
-const todoList = document.querySelector("ul");
-
-// Create new to-do
-const newTodo = document.createElement("li");
-newTodo.textContent = "Do homework";
-// Add new todo to the end of the list
-todoList.appendChild(newTodo);
