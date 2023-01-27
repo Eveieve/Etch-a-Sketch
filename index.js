48e56f18e7702a5ce18310ca06e47602a4475792
Mike <nhj25252@gmail.com>
2023-01-27 21:19:52 +0900

    Create reset button for erasing all traces

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const btn = document.querySelector("button");
-btn.setAttribute("style", "width: 100px; height: 50px");
-
+btn.setAttribute("style", "width: 150px; height: 50px");
+const resetBtn = document.querySelector(".reset");
+resetBtn.setAttribute("style", "width: 100px; height: 50px");
+resetBtn.addEventListener("click", eraseAll);
 btn.addEventListener("click", removeAll);
 btn.addEventListener("click", getGrid);
 
 let grid;
+
+function eraseAll() {
+  console.log("eraseAll");
+  const divs = document.querySelectorAll(".square");
+
+  const divsArr = Array.from(divs);
+  for (let i = 0; i < divsArr.length; i++) {
+    divsArr[i].style.background = "transparent";
+    //divsArr[i].setAttribute("style", "background: transparent;");// why is it saying it's undefined?
+  }
+}
 
 function removeAll() {
   console.log("run removeall");
